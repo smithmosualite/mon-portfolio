e71cf1922d4862d347f5d6a9af5a0521a3938c48
smithmosualite <mosualite@gmail.com>
2025-11-26 18:56:20 +0100

    before last changes

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function openTab (tabName){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 };
+
+// ====================side menu=========================
+
+let sideMenu = document.getElementById("sideMenu");
+function openMenu() {
+    sideMenu.style.right = "0"
+}
+function closeMenu() {
+    sideMenu.style.right = "-200px"
+}
