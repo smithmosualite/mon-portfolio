46d14ae02b2ff92f1443eab2c9f71d0ddbf355b8
smithmosualite <mosualite@gmail.com>
2026-01-23 17:41:17 +0100

    my last changes

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 let tablinks = document.getElementsByClassName('tab-links');
 let tabcontents = document.getElementsByClassName('tab-contents');
 
-function openTab (tabName){
-    for ( tablink of tablinks) {
+function openTab(tabName, event) {
+    for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
 
-    for (tabcontent of tabcontents) {
+    for (let tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
-};
-
+}
 // ====================side menu=========================
 
 let sideMenu = document.getElementById("sideMenu");
